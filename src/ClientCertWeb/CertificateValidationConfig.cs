d7609e8d88b704f8c5d80c0638692c39b9932796
Ozgur KARAHAN <ozkara@microsoft.com>
2017-11-23 23:50:37 +0100

    clean the older version

--- src/ClientCertWeb/CertificateValidationConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ClientCertWeb
-{
-    public class CertificateValidationConfig
-    {
-        public string Subject { get; set; }
-        public string IssuerCN { get; set; }
-        public string Thumbprint { get; set; }
-    }
-}
